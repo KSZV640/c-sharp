05affa009c5947417f351065a55a41a0cf866333
KSZV640 <85536270+KSZV640@users.noreply.github.com>
2021-07-11 13:15:29 -0600

    Add files via upload

---
+++ Ejercicios/12-ejercicio-clase-animales/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12_ejercicio_clase_animales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilidades.EnviarCorreo();
+
+            Perro p = new Perro();
+            p.Nombre = "Firulais";
+            p.Patas = 4;
+            p.EsDomestico = true;
+            p.Comer();
+            p.Caminar();
+            p.Ladrar();
+            p.Tamano();
+
+            Console.WriteLine(p.Nombre);
+            Console.WriteLine(p.Patas);
+
+            Gato g = new Gato();
+            g.Nombre = "Garfield";
+            g.Patas = 4;
+            g.Pelaje = "Peludo";
+            g.Comer();
+            g.Caminar();
+            g.Maullar();
+        }
+    }
+}
